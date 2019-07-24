31672d1cdd941e222fcf301727f1d1006d4c7aab
DeerAntlers <kolerik@hotmail.fr>
2019-07-24 17:11:31 +0200

    version 1

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+ruby '2.5.1'
+gem 'nokogiri'
+gem 'pry'
+gem 'nokogiri'
+gem 'rspec'
+gem 'rubocop'
+gem 'bundler'
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/app/game'
+require_relative 'lib/app/player'
+require_relative 'lib/app/board'
+require_relative 'lib/app/boardcase'
+require_relative 'lib/views/show'
